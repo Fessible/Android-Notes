591aa44ce2c893a769737730865a472c74c88332
rBrilliant <499953902@qq.com>
2017-07-10 20:18:25 +0800

    update animationdemo

---
+++ AnimationDemo/app/src/main/java/com/example/com/animationdemo/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.com.animationdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnAlpha(View view) {
+        //从0到1
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0, 1);
+        alphaAnimation.setDuration(1000);
+        view.startAnimation(alphaAnimation);
+    }
+
+    public void btnRotate(View view) {
+        //以(100,100)为中心旋转360
+        //RotateAnimation rotateAnimation = new RotateAnimation(90, 360,100,100);
+        //以自身为中心旋转
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360,
+                RotateAnimation.RELATIVE_TO_SELF,0.5f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
+        rotateAnimation.setDuration(1000);
+        view.startAnimation(rotateAnimation);
+    }
+
+    public void btnTranslate(View view) {
+        TranslateAnimation translateAnimation = new TranslateAnimation(0, 200, 0, 300);
+        translateAnimation.setDuration(1000);
+        view.startAnimation(translateAnimation);
+
+    }
+
+    public void btnScale(View view) {
+        //ScaleAnimation scaleAnimation = new ScaleAnimation(0, 2, 0,2);
+        //以自身为中心缩放
+        ScaleAnimation scaleAnimation = new ScaleAnimation(0, 1, 0,1, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        scaleAnimation.setDuration(1000);
+        view.startAnimation(scaleAnimation);
+    }
+
+    public void btnAnimationSet(View view) {
+        AnimationSet animationSet = new AnimationSet(true);
+        animationSet.setDuration(1000);
+
+        AlphaAnimation aa = new AlphaAnimation(0, 1);
+        aa.setDuration(1000);
+        animationSet.addAnimation(aa);
+
+        TranslateAnimation ta = new TranslateAnimation(0, 100, 0, 200);
+        ta.setDuration(1000);
+        animationSet.addAnimation(ta);
+        view.startAnimation(animationSet);
+    }
+}
