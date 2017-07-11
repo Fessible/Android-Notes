700a56b0fd1e7e5b8bd29789728ac2b9ffdf28c5
rBrilliant <499953902@qq.com>
2017-07-11 20:56:31 +0800

    update animationdemo

--- AnimationDemo/animationtest/src/main/java/com/example/com/animationtest/MainActivity.java
+++ AnimationDemo/animationtest/src/main/java/com/example/com/animationtest/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.com.animationtest;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.animation.ValueAnimator;
@@ -7,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AnimationSet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         ObjectAnimator.ofPropertyValuesHolder(view,pvh1, pvh2, pvh3).setDuration(1000).start();*/
 
        /****************ValueAnimator*********************/
-        final ValueAnimator animator = ValueAnimator.ofFloat(0, 100);
+     /*   final ValueAnimator animator = ValueAnimator.ofFloat(0, 100);
         animator.setTarget(view);
         animator.setDuration(1000).start();
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -43,7 +48,20 @@ public class MainActivity extends AppCompatActivity {
                 Float value = (Float) animator.getAnimatedValue();
                 Log.d("tag",value + " ");
             }
-        });
+        });*/
+
+     /******************AnimatorSet**************************/
+     /*   ObjectAnimator animator = ObjectAnimator.ofFloat(view, "translationX", 200);
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(view, "scaleX", 1f, 0f, 1f);
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(view, "scaleY", 1f, 0f, 1f);
+        AnimatorSet set = new AnimatorSet();
+        set.setDuration(1000);
+        set.playSequentially(animator, animator1, animator2);
+        set.start();*/
+
+        Animator animator = AnimatorInflater.loadAnimator(this, R.animator.scale);
+        animator.setTarget(view);
+        animator.start();
     }
 
     class WrapperView{
